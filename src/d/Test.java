7a742b2cc477cb8352a1b053d4a46595b07b359a
Amit Jain <amitj250@gmail.com>
2017-07-01 12:51:39 -0700

    bcd

---
+++ src/d/Test.java
@@ -0,0 +1,12 @@
+package d;
+
+public class Test {
+public static void main(String[] args) {
+	int i =3;
+	if(2==2 && i++==3){
+		System.out.println(i);
+		//i dont care
+	}
+	System.out.println(i);
+}
+}
